80c904d5f4a03c6881671780f9430a3b3e86b991
Hiren Khut <88971001+Hp92663@users.noreply.github.com>
2025-06-03 18:55:54 +0530

    cicd setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = true;
+
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? '/portfolio-main' : '',
+  assetPrefix: isGithubPages ? '/portfolio-main/' : '',
 };
 
 module.exports = nextConfig;
